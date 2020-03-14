c4029e7562c67481813a9f6b8723786c770de81d
Shinya Kumagai <roomful.rooms@gmail.com>
2020-03-15 02:17:34 +0900

    Review navigation between modules

--- account/src/main/java/com/github/droibit/android/sample/dynamicfeature/navigation/account/TwitterSignInFragment.kt
+++ account/src/main/java/com/github/droibit/android/sample/dynamicfeature/navigation/account/TwitterSignInFragment.kt
@@ -3,9 +3,11 @@ package com.github.droibit.android.sample.dynamicfeature.navigation.account
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavOptions
 import androidx.navigation.fragment.findNavController
-import com.github.droibit.android.sample.dynamicfeature.navigation.NavGraphAccountDirections.Companion.navGraphHome
+//import com.github.droibit.android.sample.dynamicfeature.navigation.NavGraphAppDirections.Companion.toHome
 import kotlinx.android.synthetic.main.fragment_twitter_sign_in.*
+import com.github.droibit.android.sample.dynamicfeature.navigation.R as appR
 
 class TwitterSignInFragment : Fragment(R.layout.fragment_twitter_sign_in) {
 
@@ -13,7 +15,12 @@ class TwitterSignInFragment : Fragment(R.layout.fragment_twitter_sign_in) {
         super.onViewCreated(view, savedInstanceState)
 
         signInButton.setOnClickListener {
-            findNavController().navigate(navGraphHome())
+            val navOptions = NavOptions.Builder()
+                .setPopUpTo(appR.id.nav_graph_app, false)
+                .build()
+            findNavController().navigate(appR.id.nav_graph_home, null, navOptions)
+
+//            findNavController().navigate(toHome())
         }
     }
 }
--- home/src/main/java/com/github/droibit/android/sample/dynamicfeature/navigation/home/HomeFragment.kt
+++ home/src/main/java/com/github/droibit/android/sample/dynamicfeature/navigation/home/HomeFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.github.droibit.android.sample.dynamicfeature.navigation.NavGraphHomeDirections.Companion.navGraphTimeline
+import com.github.droibit.android.sample.dynamicfeature.navigation.NavGraphTimelineArgs
+//import com.github.droibit.android.sample.dynamicfeature.navigation.NavGraphHomeDirections.Companion.navGraphTimeline
 import kotlinx.android.synthetic.main.fragment_home.*
+import com.github.droibit.android.sample.dynamicfeature.navigation.R as appR
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
@@ -16,10 +18,12 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         super.onViewCreated(view, savedInstanceState)
 
         timelineButton.setOnClickListener {
-            findNavController().navigate(navGraphTimeline(timelineType = 0))
+            val args = NavGraphTimelineArgs(timelineType = 0)
+            findNavController().navigate(appR.id.nav_graph_timeline, args.toBundle())
         }
         myListsButton.setOnClickListener {
-            findNavController().navigate(navGraphTimeline(timelineType = 1))
+            val args = NavGraphTimelineArgs(timelineType = 1)
+            findNavController().navigate(appR.id.nav_graph_timeline, args.toBundle())
         }
     }
 }
